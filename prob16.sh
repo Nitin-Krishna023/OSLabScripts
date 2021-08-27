be4643d4cc80bb6c2c72c65095dc791eaa7c035c
NitinKrishna023 <63190605+Nitin-Krishna023@users.noreply.github.com>
2021-08-27 05:41:29 +0000

    add prob16 and prob17

---
+++ prob16.sh
@@ -0,0 +1,28 @@
+#Write a shell script to check the spellings of any text document given as an argument
+
+file="$1"
+# Path to ispell / spell command
+SPELL="/usr/bin/ispell"
+ 
+# make sure argument is given to script
+if [ $# -eq 0 ]
+then
+	echo "Syntax: $(basename $0) file"
+	exit 1
+fi
+ 
+if [ ! -f $file ]
+then
+	echo "Error - $file not a file!"
+	exit 2
+fi
+ 
+# make sure ispell is installed
+if [ ! -x $SPELL ]
+then
+	echo "Error - $SPELL binary file does NOT exists; cannot check the spellings of $file."
+	exit 3
+fi
+ 
+# now check the spellings
+$SPELL $file
